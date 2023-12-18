2fc5b6471e2042185d63b1b6c5fb1b33ee923839
lisadhl <907629413@qq.com>
2023-12-19 00:38:17 +0900

    ADD

---
+++ Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Windows.Web.Http;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.IO.Compression;
+using Windows.UI;
+using System.Net.Http.Headers;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void Upload()
+        {
+            // https://learn.microsoft.com/en-us/dotnet/api/system.io.compression.zipfile.createfromdirectory?view=net-8.0#system-io-compression-zipfile-createfromdirectory(system-string-system-io-stream)
+            // https://stackoverflow.com/questions/16416601/c-sharp-httpclient-4-5-multipart-form-data-upload
+
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = null;
+
+            MultipartContent contents = new MultipartContent();
+
+            MultipartFormDataContent formData = new MultipartFormDataContent();
+            //formData.Add()
+            StreamContent contenet = new StreamContent();
+        }
+        public static async Task<string> Upload(byte[] image)
+        {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                using (var content =
+                    new MultipartFormDataContent("Upload----" + DateTime.Now.ToString(CultureInfo.InvariantCulture)))
+                {
+                    content.Add(new StreamContent(new MemoryStream(image)), "bilddatei", "upload.jpg");
+
+                    using (
+                       var message =
+                           await client.PostAsync("http://www.directupload.net/index.php?mode=upload", content))
+                    {
+                        var input = await message.Content.ReadAsStringAsync();
+
+                        return !string.IsNullOrWhiteSpace(input) ? Regex.Match(input, @"http://\w*\.directupload\.net/images/\d*/\w*\.[a-z]{3}").Value : null;
+                    }
+                }
+            }
+        }
+
+        public static void Upload2()
+        {
+            // https://learn.microsoft.com/en-us/dotnet/api/system.io.compression.zipfile.createfromdirectory?view=net-8.0#system-io-compression-zipfile-createfromdirectory(system-string-system-io-stream)
+            // https://stackoverflow.com/questions/16416601/c-sharp-httpclient-4-5-multipart-form-data-upload
+
+            // 保存目标
+            string webPath = "https://192.168.3.8/0000/00807/0003/";
+            // 数据来源
+            //string startPath = @".\start";
+            //string zipPath = @".\log.zip";
+            string startPath = "D://teststudy/log/";
+            string zipPath = "D://teststudy/log.zip";
+            //压缩
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+            if (!Directory.Exists(zipPath))
+            {
+                Directory.CreateDirectory(zipPath);
+            }
+            // zip转流
+            FileStream fs = File.OpenRead(zipPath);
+            var streamContent = new StreamContent(fs);
+
+            var FileContent = new ByteArrayContent(streamContent.ReadAsByteArrayAsync().Result);
+            FileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("ContentType");
+
+            var handler = new WebRequestHandler();
+            handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+            handler.ClientCertificates.Add(clientKey1);
+            handler.ServerCertificateValidationCallback = (httpRequestMessage, cert, cetChain, policyErrors) =>
+            {
+                return true;
+            };
+            using (var client = new HttpClient(handler))
+            {
+                // Post it
+                System.Net.Http.HttpResponseMessage httpResponseMessage = client.PostAsync(webPath, FileContent).Result;
+
+                if (!httpResponseMessage.IsSuccessStatusCode)
+                {
+                    string ss = httpResponseMessage.StatusCode.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ upload_json.ashx
@@ -0,0 +1,102 @@
+﻿<%@ webhandler Language="C#" class="Upload" %>
+
+
+using System;
+using System.Collections;
+using System.Web;
+using System.IO;
+using System.Globalization;
+using LitJson;
+/// <summary>
+/// 件图片上传后台一般程序
+/// </summary>
+public class Upload : IHttpHandler
+{
+	private HttpContext _context;
+
+	public void ProcessRequest(HttpContext context)
+	{
+        _context = context;
+        
+        string aspxUrl = _context.Request.Path.Substring(0, context.Request.Path.LastIndexOf("/") + 1);
+        string p = _context.Request["p"];
+        //创建临时保存路径
+        string tempSavePath = "~/Files/Attachment_Temp/";
+        if (!Directory.Exists(_context.Server.MapPath(tempSavePath)))
+        {
+            Directory.CreateDirectory(_context.Server.MapPath(tempSavePath));
+        }
+        //获取文件
+		HttpPostedFile imgFile = _context.Request.Files["imgFile"];
+		if (imgFile == null)
+		{
+			showError("请选择文件！");
+		}
+        //检测图片大小
+        int maxSize = 1024 * 1024; //最大文件大小，1MB
+		if (imgFile.InputStream == null || imgFile.InputStream.Length > maxSize)
+		{
+			showError("上传图片大小不能超过1MB！");
+		}
+        //检测图片类型
+        string fileName = imgFile.FileName;//获取文件完全限定名称
+        string fileExt = Path.GetExtension(fileName).ToLower(); //获取文件后缀名，如：.jpg
+        if (p == "0")
+        {
+            string[] extArray = { "doc", "docx" };//定义允许上传的文件扩展名
+            if (string.IsNullOrEmpty(fileExt) || Array.IndexOf(extArray, fileExt.Substring(1).ToLower()) == -1)
+            {
+                showError("只允许上传word文档！");
+            }
+        }
+        else
+        {
+            string[] extArray = { "gif", "jpg", "jpeg", "png", "bmp" };//定义允许上传的文件扩展名
+            if (string.IsNullOrEmpty(fileExt) || Array.IndexOf(extArray, fileExt.Substring(1).ToLower()) == -1)
+            {
+                showError("只允许上传gif，jpg，jpeg，png，bmp格式图片！");
+            }
+        }
+        //生成随机文件名
+        string tempFileName = System.Guid.NewGuid().ToString() + fileExt;
+        string filePath = tempSavePath + tempFileName;
+        //保存文件到服务器
+		imgFile.SaveAs(_context.Server.MapPath( filePath));
+
+        //返回保存文件路径结果集
+        string fileUrl = tempSavePath + tempFileName;
+        Hashtable result = new Hashtable(); //返回结果Hash表
+		result["error"] = 0;
+		result["url"] = fileUrl;
+        Write(JsonMapper.ToJson(result));
+	}
+
+    /// <summary>
+    /// 返回错误信息Json
+    /// </summary>
+	private void showError(string message)
+	{
+        Hashtable result = new Hashtable();//返回结果Hash表
+		result["error"] = 1;
+		result["message"] = message;
+		Write(JsonMapper.ToJson(result));
+	}
+    /// <summary>
+    /// 输出结果字字符串
+    /// </summary>
+    /// <param name="mes"></param>
+    private void Write(string mes)
+    {
+        _context.Response.AddHeader("Content-Type", "text/html; charset=UTF-8");
+        _context.Response.Write(mes);
+        _context.Response.End();
+    }
+
+	public bool IsReusable
+	{
+		get
+		{
+			return true;
+		}
+	}
+}
